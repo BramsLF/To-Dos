65fdd700b1ea85c521fd987b22ca6db9d142ce01
Brams <brayan.msanmartin@gmail.com>
2023-05-17 16:27:39 -0500

    posicion del boton en mobile

--- src/component/Createtodobutton.jsx
+++ src/component/Createtodobutton.jsx
@@ -8,14 +8,14 @@ const CreateTodoButton = () => {
   };
 
   return (
-      <button
-        className="lg:hidden fixed bottom-5 left-1/2 transform -translate-x-1/2 flex items-center justify-center w-12 rounded-full text-sky-500 hover:text-sky-50 bg-sky-50 hover:bg-sky-500 pb-2 px-4 mb-3 cursor-pointer"
-        onClick={() => handleModal()}
-      >
-        <div className="h-10 w-10 flex justify-center items-center text-center">
-          <p className="lg:hidden hover:animate-pulse text-5xl font-bold">+</p> 
-        </div>
-      </button>
+    <button
+      className="lg:hidden fixed bottom-5 left-1/2 flex items-center justify-center w-16 h-16 -ml-8 rounded-full text-sky-500 hover:text-sky-50 bg-sky-50 hover:bg-sky-500 cursor-pointer"
+      onClick={() => handleModal()}
+    >
+      <p className="w-10 mb-3 hover:animate-pulse text-6xl font-black">
+        +
+      </p>
+    </button>
   );
 };
 
