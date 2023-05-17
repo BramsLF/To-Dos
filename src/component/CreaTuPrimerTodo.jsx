2302046706cf3b95587c2eca86fb391d38c00e80
Brams <brayan.msanmartin@gmail.com>
2023-05-17 15:08:50 -0500

    retoque

--- src/component/CreaTuPrimerTodo.jsx
+++ src/component/CreaTuPrimerTodo.jsx
@@ -2,10 +2,10 @@ import nuevotodo from "../assets/icon/nuevotodo.png";
 import arrowd from "../assets/icon/arrowd.png";
 const CreaTuPrimerTodo = () => {
   return (
-    <div className="flex flex-col justify-center items-center font-lato font-black text-sky-500 text-4xl p-8 m-16">
+    <div className="flex flex-col justify-center items-center font-lato font-black text-sky-500 lg:text-4xl text-xl p-8 m-16">
         <div className="w-full flex justify-center items-center">
           <p className="">¡Crea un To-Do!</p>
-          <img src={nuevotodo} alt="nuevotodo" className="w-10"/>  
+          <img src={nuevotodo} alt="nuevotodo" className="lg:w-10 w-6"/>  
         </div>
       
       <img src={arrowd} alt="nuevotodo" className="w-10 mt-2 animate-bounce" />
